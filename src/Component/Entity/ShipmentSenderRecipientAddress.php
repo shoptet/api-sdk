21f8b4deb83393cfa76b4b67714029648a1ca0ab
robotomarvin <3751587+robotomarvin@users.noreply.github.com>
2026-05-18 09:49:25 +0200

    [create-pull-request] automated change

---
+++ src/Component/Entity/ShipmentSenderRecipientAddress.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Shoptet\Api\Sdk\Php\Component\Entity;
+
+class ShipmentSenderRecipientAddress extends Entity
+{
+    protected ?string $name;
+    protected ?string $company;
+    protected ?string $street;
+    protected ?string $city;
+    protected ?string $zip;
+    protected ?string $countryCode;
+    protected ?string $phone;
+    protected ?string $email;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?string $company): static
+    {
+        $this->company = $company;
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(?string $street): static
+    {
+        $this->street = $street;
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): static
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(?string $zip): static
+    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    public function getCountryCode(): ?string
+    {
+        return $this->countryCode;
+    }
+
+    public function setCountryCode(?string $countryCode): static
+    {
+        $this->countryCode = $countryCode;
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+        return $this;
+    }
+}
