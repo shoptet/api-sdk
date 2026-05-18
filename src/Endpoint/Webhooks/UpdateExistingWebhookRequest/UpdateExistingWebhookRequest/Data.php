72788ea36be73da48f9640e27421c80af4aa5243
suchman <4362174+suchman@users.noreply.github.com>
2026-05-18 11:25:12 +0200

    [create-pull-request] automated change

--- src/Endpoint/Webhooks/UpdateExistingWebhookRequest/UpdateExistingWebhookRequest/Data.php
+++ src/Endpoint/Webhooks/UpdateExistingWebhookRequest/UpdateExistingWebhookRequest/Data.php
@@ -6,14 +6,14 @@ use Shoptet\Api\Sdk\Php\Component\Entity\Entity;
 
 class Data extends Entity
 {
-    protected string $url;
+    protected ?string $url;
 
-    public function getUrl(): string
+    public function getUrl(): ?string
     {
         return $this->url;
     }
 
-    public function setUrl(string $url): static
+    public function setUrl(?string $url): static
     {
         $this->url = $url;
         return $this;
