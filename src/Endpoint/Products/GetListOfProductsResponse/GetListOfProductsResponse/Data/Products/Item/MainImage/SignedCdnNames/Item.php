20d021d06b07e222c54e2f5f92e0b1e85f2d31aa
AntyAnti <146710479+AntyAnti@users.noreply.github.com>
2026-08-04 07:36:31 +0200

    [create-pull-request] automated change

---
+++ src/Endpoint/Products/GetListOfProductsResponse/GetListOfProductsResponse/Data/Products/Item/MainImage/SignedCdnNames/Item.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Shoptet\Api\Sdk\Php\Endpoint\Products\GetListOfProductsResponse\GetListOfProductsResponse\Data\Products\Item\MainImage\SignedCdnNames;
+
+use Shoptet\Api\Sdk\Php\Component\Entity\Entity;
+
+class Item extends Entity
+{
+    protected string $type;
+    protected string $name;
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
