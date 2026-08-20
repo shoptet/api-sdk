c9ca4424bcf90130f220ba5071aff3767225e4c7
phodoval <9001218+phodoval@users.noreply.github.com>
2026-08-20 16:30:09 +0200

    [create-pull-request] automated change

---
+++ src/Endpoint/Products/CreateProductRequest/CreateProductRequest/Data/Variants/Item/Heureka.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Shoptet\Api\Sdk\Php\Endpoint\Products\CreateProductRequest\CreateProductRequest\Data\Variants\Item;
+
+use Shoptet\Api\Sdk\Php\Component\Entity\Entity;
+
+class Heureka extends Entity
+{
+    protected ?bool $feedExcluded;
+    protected ?bool $cartExcluded;
+
+    public function getFeedExcluded(): ?bool
+    {
+        return $this->feedExcluded;
+    }
+
+    public function setFeedExcluded(?bool $feedExcluded): static
+    {
+        $this->feedExcluded = $feedExcluded;
+        return $this;
+    }
+
+    public function getCartExcluded(): ?bool
+    {
+        return $this->cartExcluded;
+    }
+
+    public function setCartExcluded(?bool $cartExcluded): static
+    {
+        $this->cartExcluded = $cartExcluded;
+        return $this;
+    }
+}
