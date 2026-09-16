0120c06ee3a0e85d54ae46726802c47252994493
phodoval <9001218+phodoval@users.noreply.github.com>
2026-09-16 11:32:23 +0200

    [create-pull-request] automated change

--- src/Component/Entity/ProductUpdate/Variants/Item/StocksLocations/Item.php
+++ src/Component/Entity/ProductUpdate/Variants/Item/StocksLocations/Item.php
@@ -3,10 +3,12 @@
 namespace Shoptet\Api\Sdk\Php\Component\Entity\ProductUpdate\Variants\Item\StocksLocations;
 
 use Shoptet\Api\Sdk\Php\Component\Entity\Entity;
+use Shoptet\Api\Sdk\Php\Component\ValueObject\TypeAmountNullable;
 
 class Item extends Entity
 {
     protected int $stockId;
+    protected ?TypeAmountNullable $amount;
     protected ?string $location;
 
     public function getStockId(): int
@@ -17,6 +19,17 @@ class Item extends Entity
     public function setStockId(int $stockId): static
     {
         $this->stockId = $stockId;
+        return $this;
+    }
+
+    public function getAmount(): ?TypeAmountNullable
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(?TypeAmountNullable $amount): static
+    {
+        $this->amount = $amount;
         return $this;
     }
 
